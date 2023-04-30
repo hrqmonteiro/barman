abee0499c5cf216f4762df6667cd40d979045d17
hrqmonteiro <hrqmonteiro@protonmail.com>
2023-04-30 20:35:54 -0300

    refactor: improving panel areas items processing and icon instantiation

--- extension.js
+++ extension.js
@@ -1,12 +1,18 @@
-const { Gio, St } = imports.gi;
-const { main: Main, panelMenu: PanelMenu } = imports.ui;
-const panel = Main.panel;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const extensionName = Me.metadata.name;
 const { TOGGLE_STATUS } = Me.imports.enums.ToggleStatus;
+const { Gio, GObject, St } = imports.gi;
+const { main: Main, panelMenu: PanelMenu } = imports.ui;
+const panel = Main.panel;
 
-let icon;
+const ClosedIcon = 'closed-symbolic';
+const OpenedIcon = 'opened-symbolic';
+
+let icon = new St.Icon({
+  gicon: Gio.icon_new_for_string(`${Me.path}/icons/${OpenedIcon}.svg`),
+  style_class: 'system-status-icon'
+});;
 let itemsToHide = [];
 let panelButton = null;
 let Settings = null;
@@ -14,8 +20,6 @@ let toggleStatus = TOGGLE_STATUS.INACTIVE;
 
 // TODO add support to: logoMenu, Replace Activities, Bluetooth Battery Indicator, Aylur's widgets date and morere
 const excludedPropertyNames = ['a11y', 'activities', 'appMenu', 'dateMenu', 'dwellClick', 'keyboard', 'screenRecording', 'screenSharing', 'uniteDesktopLabel'];
-const ClosedIcon = 'closed-symbolic';
-const OpenedIcon = 'opened-symbolic';
 
 const excludedItems = Object.keys(panel.statusArea).reduce((result, key) => {
   if (excludedPropertyNames.includes(key)) {
@@ -25,23 +29,6 @@ const excludedItems = Object.keys(panel.statusArea).reduce((result, key) => {
   return result;
 }, []);
 
-const panelBoxs = [
-  panel._leftBox.get_children(),
-  panel._centerBox.get_children(),
-  panel._rightBox.get_children()
-];
-
-function processItems (items) {
-  for (let i = 0; i < items.length; i++) {
-    const item = items[i];
-    const child = item.get_child();
-
-    if (!excludedItems.includes(child)) {
-      itemsToHide.push(child);
-    }
-  }
-};
-
 function toggleItems(items, mode) {
   const action = mode === 'hide' ? 'hide' : 'show';
   for (let i = 0; i < items.length; i++) {
@@ -50,8 +37,52 @@ function toggleItems(items, mode) {
   }
 }
 
+function processLeftBox() {
+  const leftBox = panel._leftBox.get_children();
+
+  for (let i = 0; i < leftBox.length; i++) {
+    const item = leftBox[i];
+    const child = item.get_child();
+
+    
+    if (!excludedItems.includes(child) && child !== panel.statusArea['barmanButton']) {
+      itemsToHide.push(child);
+    }
+  }
+}
+
+function processCenterBox() {
+  const centerBox = panel._centerBox.get_children();
+
+  for (let i = 0; i < centerBox.length; i++) {
+    const item = centerBox[i];
+    const child = item.get_child();
+
+    
+    if (!excludedItems.includes(child) && child !== panel.statusArea['barmanButton']) {
+      itemsToHide.push(child);
+    }
+  }
+}
+
+function processRightBox() {
+  const rightBox = panel._rightBox.get_children();
+
+  for (let i = 0; i < rightBox.length; i++) {
+    const item = rightBox[i];
+    const child = item.get_child();
+
+    
+    if (!excludedItems.includes(child) && child !== panel.statusArea['barmanButton']) {
+      itemsToHide.push(child);
+    }
+  }
+}
+
 function toggleBarman() {
-  panelBoxs.forEach(processItems);
+  processLeftBox();
+  processCenterBox();
+  processRightBox();
   toggleItems(itemsToHide, 'hide');
 
   if (toggleStatus === TOGGLE_STATUS.INACTIVE) {
@@ -62,10 +93,6 @@ function toggleBarman() {
     toggleStatus = TOGGLE_STATUS.INACTIVE;
     toggleItems(itemsToHide, 'show');
     icon.gicon = Gio.icon_new_for_string(`${Me.path}/icons/${OpenedIcon}.svg`)
-
-    setTimeout(_ => {
-      toggleBarman();
-    }, 10000)
   }
 }
 
@@ -83,8 +110,7 @@ function getPanelButton() {
 }
 
 function addButton() {
-  const indicatorName = `${extensionName} Indicator`;
-  panel.addToStatusArea(indicatorName, getPanelButton(), 90);
+  panel.addToStatusArea('barmanButton', getPanelButton(), 99);
 }
 
 function removeButton() {
@@ -96,26 +122,12 @@ function removeButton() {
 
 function init() {
   log(`initializing ${extensionName}`);
-  icon = new St.Icon({
-    gicon: Gio.icon_new_for_string(`${Me.path}/icons/${OpenedIcon}.svg`),
-    style_class: 'system-status-icon'
-  });
 }
 
 function enable() {
-  icon = new St.Icon({
-    gicon: Gio.icon_new_for_string(`${Me.path}/icons/${OpenedIcon}.svg`),
-    style_class: 'system-status-icon'
-  });
-  Settings = ExtensionUtils.getSettings();
-  Settings.connect('changed::panel-position', () => {
-    removeButton();
-    addButton();
-  })
   addButton();
 }
 
 function disable() {
-  Settings = null;
   removeButton();
 }
